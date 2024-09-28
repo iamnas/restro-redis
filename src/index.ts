0090be3f834ecdd47943f2217a8609d580e7a1e6
iamnas <iamnas.eth@gmail.com>
2024-09-29 02:42:55 +0530

    setup express

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import restaurantsRouter from "./routes/restaurants";
+import cuisinesRouter from "./routes/cuisines"
+import { errorHandler } from './middlewarea/errorHandler';
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/v1/restaurants",restaurantsRouter);
+app.use("/api/v1/cuisines",cuisinesRouter);
+
+
+
+app.use(errorHandler);
+app
+  .listen(PORT, () => {
+    console.log(`Application listening on ${PORT}`);
+  })
+  .on("error", (err) => {
+    throw new Error(err.message);
+  });
---
+++ src/routes/restaurants.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+
+    res.status(200).json({
+        message:"Success"
+    })
+
+});
+
+export default router;
